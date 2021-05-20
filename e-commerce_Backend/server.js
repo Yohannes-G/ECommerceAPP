5a2d379844c728018c3f1b34d53b90ec5c249184
mannyGirmaw <yohannesgirmaw23@gmail.com>
2021-05-20 10:24:35 +0300

    order routes added

--- e-commerce_Backend/server.js
+++ e-commerce_Backend/server.js
@@ -4,7 +4,8 @@ import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './connection/database.js'
 import productRoutes from './routes/productRoutes.js'
-
+import  userRoutes from './routes/userRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
 import {notFound, errorHandler} from './middleware/errorMiddleWare.js'
 //import files from the .env
 dotenv.config()
@@ -14,15 +15,18 @@ connectDB()
 
 const app = express()
  
-
-
+//to accept json from the body for user {email, password}
+app.use(express.json())
  
 app.get('/', (req, res)=>{
 	res.send('API is running....')
 
 })
 
-app.use('/api/products', productRoutes)
+app.use('/api/products', productRoutes) 
+app.use('/api/users', userRoutes)
+app.use('/api/orders',orderRoutes)
+//app.use('/api/users',userRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
@@ -31,4 +35,4 @@ app.use(errorHandler)
  
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Hey Jo The Backend is listening at http://localhost:${PORT}`.yellow.bold))
- 
+  
