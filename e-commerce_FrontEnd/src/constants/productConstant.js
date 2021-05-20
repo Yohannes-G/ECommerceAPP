4a54aa13944bb58d2d2eb7705fc2ccdd5ca2f0cb
mannyGirmaw <yohannesgirmaw23@gmail.com>
2021-05-20 10:29:09 +0300

    css for index

--- e-commerce_FrontEnd/src/constants/productConstant.js
+++ e-commerce_FrontEnd/src/constants/productConstant.js
@@ -1,3 +1,13 @@
+//steps for redux
+/*
+1-Create constant action types
+2-Create reducer function
+3-Set the reducer in Store component, combined reducer method
+4-then dispatch the action to the requesting component
+
+*/
+
+
 export const PRODUCT_LIST_REQUEST = 'PRODUCT_LIST_REQUEST'
 export const PRODUCT_LIST_SUCCESS = 'PRODUCT_LIST_SUCCESS'
 export const PRODUCT_LIST_FAIL = 'PRODUCT_LIST_FAIL'
@@ -5,3 +15,4 @@ export const PRODUCT_LIST_FAIL = 'PRODUCT_LIST_FAIL'
 export const PRODUCT_DETAILS_REQUEST = 'PRODUCT_DETAILS_REQUEST'
 export const PRODUCT_DETAILS_SUCCESS = 'PRODUCT_DETAILS_SUCCESS'
 export const PRODUCT_DETAILS_FAIL = 'PRODUCT_DETAILS_FAIL'
+
