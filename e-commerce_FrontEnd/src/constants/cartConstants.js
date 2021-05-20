a7ec73978d281e8ef516856c25d3149569a0236e
mannyGirmaw <yohannesgirmaw23@gmail.com>
2021-05-20 10:39:46 +0300

    cart constants added

---
+++ e-commerce_FrontEnd/src/constants/cartConstants.js
@@ -0,0 +1,11 @@
+/*cart save the items they want to buy, 
+change the quantity, 
+and remove them. 
+It should also be able to show the total cost of 
+items in the cart.
+
+*/
+export const CART_ADD_ITEM = 'CART_ADD_ITEM'
+export const CART_REMOVE_ITEM = 'CART_REMOVE_ITEM'
+export const CART_SAVE_SHIPPING_ADDRESS='CART_SAVE_SHIPPING_ADDRESS'
+export const CART_SAVE_PAYMENT_METHOD = 'CART_SAVE_PAYMENT_METHOD'
